48510f2faa14372c5fb66df179800f5c30f52699
Philip Warren <philwarren1@gmail.com>
2020-08-09 17:09:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,18 +11,3 @@ class Song
   end
 
 end
-def self.new_from_csv(csv_data)
-    rows = csv_data.split("\n")
-    people = rows.collect do |row|
-      data = row.split(", ")
-      name = data[0]
-      age = data[1]
-      company = data[2]
- 
-      person = self.new
-      person.name = name
-      person.age = age
-      person.company = company
-      person
-    end
-    people
